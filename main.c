e99812558df779ecf5e579d13ecab313092e53a9
public-jun <generalpublicjunjun@gmail.com>
2021-04-09 21:27:21 +0900

    test_ft_strcpy

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jnakahod <jnakahod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/07 13:33:11 by jnakahod          #+#    #+#             */
-/*   Updated: 2021/04/07 16:11:22 by jnakahod         ###   ########.fr       */
+/*   Updated: 2021/04/09 21:08:08 by jnakahod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,54 @@
 #include <string.h>
 #include <stdlib.h>
 
-size_t ft_strlen(char *s);
+size_t	ft_strlen(char *s);
+char	*ft_strcpy(char *dst, const char *src);
+
+static char	*get_char_p_malloc(char *src)
+{
+	size_t	size;
+	char	*res;
+
+	size = strlen(src);
+	res = (char *)malloc(sizeof(char) * (size + 1));
+	if (!(res))
+	{
+		printf("error\n");
+		exit(0);
+	}
+	res[size] = '\0';
+	return (res);
+}
+
+int		test_ft_strcpy(char *src)
+{
+	char *dst;
+
+	dst = get_char_p_malloc(src);
+	printf("\x1b[44m------ft_strcpy------\033[m\n");
+	ft_strcpy(dst, src);
+	printf("ft_strcpy		:%s\n", dst);
+	free(dst);
+	dst = NULL;
+	dst = get_char_p_malloc(src);
+	strcpy(dst, src);
+	printf("ft_strcpy		:%s\n", dst);
+	free(dst);
+	dst = NULL;
+	printf("\x1b[34mnull_test\033[m\n");
+	dst = get_char_p_malloc(src);
+	ft_strcpy(dst, "");
+	printf("ft_strcpy		:%s\n", dst);
+	free(dst);
+	dst = NULL;
+	dst = get_char_p_malloc(src);
+	strcpy(dst, "");
+	printf("ft_strcpy		:%s\n", dst);
+	free(dst);
+	dst = NULL;
+	while(1);
+	return (0);
+}
 
 
 int		test_ft_strlen(char *src)
@@ -32,7 +79,7 @@ int		test_ft_strlen(char *src)
 	}
 	s[size] = '\0';
 	strcpy(s, src);
-	printf("\x1b[44m------ft_stlen------\033[m\n");
+	printf("\x1b[44m------ft_strlen------\033[m\n");
 	res = 0;
 	res = ft_strlen(s);
 	printf("ft_strlen		:%d\n", res);
@@ -60,6 +107,17 @@ int	main(int ac, char **argv)
 	{
 		if (argv[2])
 			return (test_ft_strlen(argv[2]));
+		else
+		{
+			printf("error\n");
+			return (-1);
+		}
+			
+	}
+	if (!(strncmp(argv[1], "ft_strcpy", 10)))
+	{
+		if (argv[2])
+			return (test_ft_strcpy(argv[2]));
 		else
 		{
 			printf("error\n");
